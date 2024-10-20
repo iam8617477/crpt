1bced36e821bfdf175023929c38517f99721c8fc
iam8617477 <iam8617477@gmail.com>
2024-10-20 22:35:08 +0300

    refactor: add encrypt for bytes

--- sync_encryptor.py
+++ sync_encryptor.py
@@ -23,25 +23,36 @@ class SyncEncryptor:
         )
         return base64.urlsafe_b64encode(kdf.derive(self.passphrase))
 
-    def encrypt(self, plain_text: str, use_salt: bool = False) -> (str, str):
+    def encrypt(self, data, use_salt: bool = False, is_bytes: bool = False) -> (str, str):
+        salt = None
         if use_salt:
             salt = self._generate_salt()
             key = self._generate_key(salt)
-            cipher = Fernet(key)
-            encrypted_text = cipher.encrypt(plain_text.encode())
-            return encrypted_text.decode(), base64.urlsafe_b64encode(salt).decode()
         else:
             key = self._generate_key()
-            cipher = Fernet(key)
-            encrypted_text = cipher.encrypt(plain_text.encode())
-            return encrypted_text.decode(), None
 
-    def decrypt(self, encrypted_text: str, salt: str = None) -> str:
+        cipher = Fernet(key)
+
+        if not is_bytes:
+            data = data.encode()
+
+        encrypted_data = cipher.encrypt(data)
+
+        return (encrypted_data.decode() if not is_bytes else encrypted_data,
+                base64.urlsafe_b64encode(salt).decode() if use_salt else None)
+
+    def decrypt(self, encrypted_data, salt: str = None, is_bytes: bool = False) -> (str, bytes):
         if salt:
             salt = base64.urlsafe_b64decode(salt)
             key = self._generate_key(salt)
         else:
             key = self._generate_key()
+
         cipher = Fernet(key)
-        decrypted_text = cipher.decrypt(encrypted_text.encode()).decode()
-        return decrypted_text
+
+        if not is_bytes:
+            encrypted_data = encrypted_data.encode()
+
+        decrypted_data = cipher.decrypt(encrypted_data)
+
+        return decrypted_data if is_bytes else decrypted_data.decode()
